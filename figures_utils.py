6cfe19f47fc896c91fa6d6473ef92a0458cb965b
mas <mas@thorium>
2025-03-17 00:07:21 -0400

    Trying to figure out the right structure

--- figures_utils.py
+++ figures_utils.py
@@ -1,1 +1,7 @@
-import plotly.graph_obs as go
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+import plotly.express as px
+
+from plotly.subplots import make_subplots
+from config import config as cfg
