ce3a1519e93495bbdc5c12860f7ca183030711af
mas <mas@thorium>
2025-03-17 00:06:46 -0400

    data preprcoessing

--- data/data_endpoints.py
+++ data/data_endpoints.py
@@ -1,3 +1,4 @@
-nyc_NFHDM = "https://data.cityofnewyork.us/resource/r3dx-pew9.json"
-nyc_ARREST = "https://data.cityofnewyork.us/resource/uip8-fykc.json" # (2.6 mm rows)
-nyc_DOG = "https://data.cityofnewyork.us/resource/rsgh-akpg.json" # (29k rows
+nyc_NFHDM = "https://data.cityofnewyork.us/resource/r3dx-pew9.json"  # Target
+nyc_ARREST = "https://data.cityofnewyork.us/resource/uip8-fykc.json"
+nyc_PUMA = "https://data.cityofnewyork.us/resource/pikk-p9nv.json"
+nyc_DOG = "https://data.cityofnewyork.us/resource/rsgh-akpg.json"  # (29k rows
