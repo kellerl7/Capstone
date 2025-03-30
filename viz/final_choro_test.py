4ef4bb8578ac40ae1121fd39cb03cbfa4b6249d4
mas <mas@thorium>
2025-03-30 07:34:11 -0400

    feat: testing main visualization

---
+++ viz/final_choro_test.py
@@ -0,0 +1,33 @@
+import plotly.express as px
+import plotly.graph_objs as go
+import json
+import geopandas as gpd
+import pandas as pd
+
+import unsupervised.data_preprocess as data_pre
+
+# Load in our zipcode data
+with open('data/ny_new_york_zip_codes_geo.min.json', 'rb') as f:
+    nyc_zips = json.load(f)
+
+# Load in data we want to show
+map_df, loadings = data_pre.main(
+    'data/public_fac.csv',
+    'data/prop_values.csv',
+    n_pca=15,
+    k_cluster=5)
+
+# Define our mapping variables
+map_json = nyc_zips
+map_idkey = 'properties.ZCTA5E10'
+
+# Create our map
+fig = px.choropleth_map(
+    map_df,
+    geojson=map_json,
+    featureidkey=map_idkey,
+    locations='zip',
+    color='revised_market_value',
+    map_style='outdoors'
+)
+fig.show()
