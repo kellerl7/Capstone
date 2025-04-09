cae8bc04ce34e04e4537511eaec7a313a04a961d
mas <mas@thorium>
2025-04-08 21:57:52 -0400

    Property value endpoint

--- src/data_endpoints.py
+++ src/data_endpoints.py
@@ -1,4 +1,5 @@
 nyc_NFHDM = "https://data.cityofnewyork.us/resource/r3dx-pew9.json"  # Target
 nyc_ARREST = "https://data.cityofnewyork.us/resource/uip8-fykc.json"
 nyc_PUMA = "https://data.cityofnewyork.us/resource/pikk-p9nv.json"
-nyc_DOG = "https://data.cityofnewyork.us/resource/rsgh-akpg.json"  # (29k rows
+nyc_DOG = "https://data.cityofnewyork.us/resource/rsgh-akpg.json"  # 29k rows
+nyc_RNOPV = "https://data.cityofnewyork.us/resource/8vgb-zm6e.json" # 27.3k rows
