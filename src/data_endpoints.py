cce4e9abe34c41fff821545f58a0d99c04b10e80
mas <mas@thorium>
2025-04-09 23:11:18 -0400

    data storage

--- src/data_endpoints.py
+++ src/data_endpoints.py
@@ -3,3 +3,4 @@ nyc_ARREST = "https://data.cityofnewyork.us/resource/uip8-fykc.json"
 nyc_PUMA = "https://data.cityofnewyork.us/resource/pikk-p9nv.json"
 nyc_DOG = "https://data.cityofnewyork.us/resource/rsgh-akpg.json"  # 29k rows
 nyc_RNOPV = "https://data.cityofnewyork.us/resource/8vgb-zm6e.json" # 27.3k rows
+nyc_HIST_ARREST = "https://data.cityofnewyork.us/resource/8h9b-rp9u.json" #!! 5.7 MM
